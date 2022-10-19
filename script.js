fc73d9a46ede38dd187f557c855f4b0b4c1f99ff
Angelina Julia <ajuliammelo@gmail.com>
2022-10-19 13:15:16 -0300

    requisição api

---
+++ script.js
@@ -0,0 +1,29 @@
+const cep = document.getElementById("cep")
+const rua = document.getElementById("rua")
+const bairro = document.getElementById("bairro")
+const cidade = document.getElementById("cidade")
+const estado = document.getElementById("estado")
+
+const currentCEP= async (cepValue) => {
+    
+    const data = await fetch(`https://cdn.apicep.com/file/apicep/${cepValue}.json`)
+    .then(response => response.json())
+    .catch(function(err){
+        console.log(err)
+        alert('Cep não encontrado. Digite o cep usando o separador"-", ex: 00000-000')
+
+    })
+
+    rua.value=data.address
+    bairro.value = data.district
+    cidade.value = data.city
+    estado.value = data.state
+    
+}
+
+cep.addEventListener('blur', e =>{
+    if(e.target.value !== ""){
+    let cepValue=e.target.value
+    currentCEP(cepValue)
+    }
+})
